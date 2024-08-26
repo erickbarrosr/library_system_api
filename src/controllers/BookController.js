0c7a4944d6bfee8070e28b6e8b91c403ae3052e8
Erick Rezende <erick@sensio.com.br>
2024-08-26 18:49:15 -0300

    feat(BookController.js): created book controller

---
+++ src/controllers/BookController.js
@@ -0,0 +1,34 @@
+import Book from "../models/Book.js";
+import Livro from "../models/Book.js";
+
+class BookController {
+  static async registerBook(req, res) {
+    try {
+      const body = req.body;
+
+      const newBook = await Livro.create(body);
+
+      res.status(201).json({ message: "Livro criado com sucesso!", newBook });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - Erro ao registrar livro.` });
+    }
+  }
+
+  static async getBooks(req, res) {
+    try {
+      const books = await Book.find({});
+
+      res.status(200).json({ message: "Livros encontrados!", books });
+    } catch (error) {}
+  }
+
+  static async getOneBook(req, res) {}
+
+  static async updateBook(req, res) {}
+
+  static async deleteBook(req, res) {}
+}
+
+export default BookController;
