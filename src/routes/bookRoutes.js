298ac94df235b5d9d90dc9590b6d4c4a1cc4e276
Erick Rezende <erick@sensio.com.br>
2024-08-28 16:13:36 -0300

    feat(bookRoutes.js): created all routes for books

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -3,6 +3,11 @@ import BookController from "../controllers/BookController.js";
 
 const router = express.Router();
 
-router.post("/book", BookController.registerBook);
+router.post("/register", BookController.registerBook);
+router.get("/books", BookController.getBooks);
+router.get("/book/:id", BookController.getBookById);
+router.put("/book/:id", BookController.updateBook);
+router.delete("/book/:id", BookController.deleteBook);
+router.get("/books/query", BookController.titleQuery);
 
 export default router;
