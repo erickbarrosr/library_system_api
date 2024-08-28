c8d08f9b9f94df7b21f76fc13ae8eb1b5a354113
Erick Rezende <erick@sensio.com.br>
2024-08-28 16:13:07 -0300

    feat(BookController.js): finished methods to books

--- src/controllers/BookController.js
+++ src/controllers/BookController.js
@@ -4,11 +4,11 @@ import Livro from "../models/Book.js";
 class BookController {
   static async registerBook(req, res) {
     try {
-      const book = new Book(req.body);
+      const newBook = new Book(req.body);
 
-      const newBook = await book.save();
+      const savedBook = await newBook.save();
 
-      res.status(201).json({ message: "Livro criado com sucesso!", newBook });
+      res.status(201).json({ message: "Livro criado com sucesso!", savedBook });
     } catch (error) {
       res
         .status(500)
@@ -21,14 +21,70 @@ class BookController {
       const books = await Book.find({});
 
       res.status(200).json({ message: "Livros encontrados!", books });
-    } catch (error) {}
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - Erro ao listar livros.` });
+    }
   }
 
-  static async getOneBook(req, res) {}
+  static async getBookById(req, res) {
+    try {
+      const id = req.params.id;
 
-  static async updateBook(req, res) {}
+      const book = await Book.findById(id);
 
-  static async deleteBook(req, res) {}
+      res.status(200).json({ message: "Livro encontrado!", book });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - Erro ao listar livro.` });
+    }
+  }
+
+  static async updateBook(req, res) {
+    try {
+      const id = req.params.id;
+
+      const book = await Book.findByIdAndUpdate(id, { $set: req.body });
+
+      const updatedBook = await Book.find({ _id: book._id });
+
+      res.status(200).json({ message: "Livro atualizado!", updatedBook });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - Erro ao atualizar livro.` });
+    }
+  }
+
+  static async deleteBook(req, res) {
+    try {
+      const id = req.params.id;
+
+      const book = await Book.findByIdAndDelete(id);
+
+      res.status(200).json({ message: "Livro removido!", book });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - Erro ao remover livro.` });
+    }
+  }
+
+  static async titleQuery(req, res) {
+    try {
+      const query = req.query.title;
+
+      const response = await Book.find({ title: query });
+
+      res.status(200).json({ message: "Livro encontrado!", response });
+    } catch (error) {
+      res.status(500).json({
+        message: `${error.message} - Erro ao buscar livro pelo título.`,
+      });
+    }
+  }
 }
 
 export default BookController;
